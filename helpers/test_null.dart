337657051d6534dbc38034b403e986c1665a86ac
Allan Silva <sil.allanlucas@gmail.com>
2024-01-11 11:46:02 -0300

    resolving merge conflicts

--- helpers/test_null.dart
+++ helpers/test_null.dart
@@ -9,11 +9,7 @@ void testingNullSafety(){
   Random rng = Random();
   int randomNumber = rng.nextInt(10);
   if (randomNumber <= 5){
-<<<<<<< HEAD
     myAccount = Account(name: 'Ricarth', balance: 200, isAuthenticated: true);
-=======
-    myAccount = Account(name: 'ComercialInterno', balance: 200, isAuthenticated: true);
->>>>>>> 86342e45a4c589420083abcd2a902662325fe8d6
   }
 
   print(myAccount != null ? myAccount.balance : 'Conta Nula!');
